e4229d6c9c84fa780c3908a2692832a8f36eb566
Abdul Aziz A.B <66479632+4bdul4ziz@users.noreply.github.com>
2022-01-08 16:57:01 +0530

    front end home

--- FrontEnd/page.php
+++ FrontEnd/page.php
@@ -21,7 +21,7 @@ if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !==true)
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-    <title>Employe web</title>
+    <title>Employee Portal</title>
   </head>
   <body>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
